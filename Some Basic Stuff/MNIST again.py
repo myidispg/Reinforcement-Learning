2fc17e79eb52ae700e5fa12970d26ee5d80c264c
Prashnt Goyal <myidispg@gmail.com>
2018-12-05 20:49:52 +0530

    Completed MNIST again

--- Some Basic Stuff/MNIST again.py
+++ Some Basic Stuff/MNIST again.py
@@ -35,4 +35,72 @@ for i in range(cols):
             axs[j][i].set_title(str(j))
             num_of_samples.append(len(x_selected))
 
- 
+print(num_samples)
+plt.figure(figsize(12, 4))
+plt.bar(range(0, num_classes), num_of_samples)
+plt.title("Distribution of the training dataset")
+plt.xlabel("Class number")
+plt.ylabel("Number of images")
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+X_train = X_train/255 
+X_test = X_test/255
+
+num_pixels = 784
+X_train = X_train.reshape(X_train.shape[0], num_pixels)
+X_test = X_test.reshape(X_test.shape[0], num_pixels)
+def create_model():
+    model = Sequential()
+    model.add(Dense(10, input_dim=num_pixels, activation='relu'))
+    model.add(Dense(30, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(Adam(lr=0.01), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+model = create_model()
+print(model.summary())
+
+history = model.fit(X_train, y_train, validation_split=0.1, epochs = 10, batch_size = 200, verbose = 1, shuffle = 1)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['loss', 'val_loss'])
+plt.title('Loss')
+plt.xlabel('epoch')
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.legend(['acc', 'val_acc'])
+plt.title('Accuracy')
+plt.xlabel('epoch')
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print(type(score))
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+import requests
+from PIL import Image
+
+url = 'https://www.researchgate.net/profile/Jose_Sempere/publication/221258631/figure/fig1/AS:305526891139075@1449854695342/Handwritten-digit-2.png'
+response = requests.get(url, stream=True)
+img = Image.open(response.raw)
+plt.imshow(img, cmap=plt.get_cmap('gray'))
+
+import cv2
+
+img = np.asarray(img)
+img = cv2.resize(img, (28, 28))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.bitwise_not(img)
+plt.imshow(img, cmap=plt.get_cmap('gray'))
+
+img = img/255
+img = img.reshape(1, 784)
+
+prediction = model.predict_classes(img)
+print("predicted digit:", str(prediction))
