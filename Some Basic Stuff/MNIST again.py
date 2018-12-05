cafc817133c0cdc7d20f3a42d02a8f317db1f34f
Prashnt Goyal <myidispg@gmail.com>
2018-12-05 20:45:42 +0530

    Working on MNIST again

--- Some Basic Stuff/MNIST again.py
+++ Some Basic Stuff/MNIST again.py
@@ -5,3 +5,34 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras.utils.np_utils import to_categorical
+import random
+
+np.random.seed(0)
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+assert(X_train.shape[0] == y_train.shape[0]), "The number of images is not equal to the number of labels"
+assert(X_test.shape[0] == y_test.shape[0]), "The number of test images is not equal to the number of labels"
+assert(X_train.shape[1:] == (28, 28)), "The image dimensions are incorrect"
+assert(X_test.shape[1:] == (28, 28)), "The test image dimensions are incorrect"
+
+num_of_samples = []
+
+cols = 5
+num_classes = 10
+
+fig, axs = plt.subplots(nrows=num_classes, ncols = cols, figsize=(5, 5))
+fig.tight_layout()
+
+for i in range(cols):
+    for j in range(num_classes):
+        x_selected = X_train[y_train == j]
+        axs[j][i].imshow(x_selected[random.randint(0, len(x_selected - 1)), :, :], 
+           cmap=plt.get_cmap('gray'))
+        axs[j][i].axis('off')
+        if i == 2:
+            axs[j][i].set_title(str(j))
+            num_of_samples.append(len(x_selected))
+
+ 
