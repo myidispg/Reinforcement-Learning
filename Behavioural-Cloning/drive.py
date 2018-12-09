47daf0f540358e090710f297d82f9aed83ce0fd6
Prashnt Goyal <myidispg@gmail.com>
2018-12-09 21:07:03 +0530

    Working

---
+++ Behavioural-Cloning/drive.py
@@ -0,0 +1,24 @@
+import numpy as mp
+import pandas as pd
+import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
+import cv2
+import os
+import random
+import ntpath
+
+datadir = 'Data/'
+columns = ['center', 'left', 'right', 'steering', 'reverse', 'speed']
+data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'), names=columns)
+pd.set_option('display.max_colwidth', -1)
+data.head()
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail
+data['center'] = data['center'].apply(path_leaf)
+data['left'] = data['left'].apply(path_leaf)
+data['right'] = data['right'].apply(path_leaf)
