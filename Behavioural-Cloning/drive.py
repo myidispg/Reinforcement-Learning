fb437ec8d7e8d0372fd87481de8489be712bd7b6
myidispg <myidispg@gmail.com>
2018-12-13 20:55:04 +0530

    Working on connecting to simulator

---
+++ Behavioural-Cloning/drive.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 13 19:51:24 2018
+
+@author: myidi
+"""
+
+from flask import Flask
+import socketio
+import eventlet
+
+sio = socketio.Server()
+
+app = Flask(__name__) #'__main__'
+
+@app.route('/home')
+def greeting():
+    return 'Welcome'
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    send_control(0, 1)
+
+def send_control(steering_angle, throttle):
+    sio.emit('steer', data={
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
+    })
+
+if __name__ == '__main__':
+    # apsp.run(port=3000)
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
