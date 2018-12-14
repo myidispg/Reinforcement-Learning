1945c80a9113c6f96c0c8c9ad650d039fcd6eee3
myidispg <myidispg@gmail.com>
2018-12-14 21:28:27 +0530

    Conducted a test. Failed horribly. Needs some more work

--- Behavioural-Cloning/drive.py
+++ Behavioural-Cloning/drive.py
@@ -9,11 +9,37 @@ from flask import Flask
 import socketio
 import eventlet
 from keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import cv2
 
-sio = socketio.Server()
+sio = socketio.Server() 
 
 app = Flask(__name__) #'__main__'
+speed_limit = 10
+# Preprocessing the data
+def img_preprocess(img):
+    img = img[60:135, :, :]
+    # COnvert to YUV ColorSpace
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3,3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
 
+@sio.on('telemetry')
+def telemetry(sid, data):
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = img_preprocess(image)
+    image = np.array([image])
+    steering_angle = float(model.predict(image))
+    speed = float(data['speed'])
+    throttle = 1.0 - speed/speed_limit
+    print('{} {} {}'.format(steering_angle, throttle, speed))
+    send_control(steering_angle, throttle)
 
 @sio.on('connect')
 def connect(sid, environ):
